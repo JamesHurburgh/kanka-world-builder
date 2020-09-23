1fc24976c5b1bd36081e7023ab2dbbe302ba3f57
James Hurburgh <JamesHurburgh@gmail.com>
2020-09-23 10:12:00 +1000

    config: Set better defaults

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 options = {
-                            'debug' : True,
-                           'active' : True, # Will hit the Kanka api and create entities instead of just parsing them.
+                            'debug' : False,
+                           'active' : False, # Will hit the Kanka api and create entities instead of just parsing them.
                          'heraldry' : False, # Will get heraldry from ironarachne.com and include in entities where appropriate 
                        'attributes' : False, # Will parse attributes on entities, not just the entities themselves.
                    'entities.world' : True, # This should always be true, otherwise many other things will fail.
-               'entities.timelines' : False, # Parse Timelines
-                  'entities.biomes' : False, # Parse Biomes
-                  'entities.rivers' : False, # Parse Rivers
-                  'entities.states' : False, # Parse States
-        'entities.states.neighbors' : False, # Parse relationships for States neighbor data.
-        'entities.states.diplomacy' : False, # Parse relationships for States diplomacy data.
-    'entities.states.diplomacy.all' : False, # Parse relationships for Neutral and Unknown diplomacies.
-          'entities.states.history' : False, # Will parse history where appropriate 
-               'entities.provinces' : False, # Parse Provinces
+               'entities.timelines' : True, # Parse Timelines
+                  'entities.biomes' : True, # Parse Biomes
+                  'entities.rivers' : True, # Parse Rivers
+                  'entities.states' : True, # Parse States
+        'entities.states.neighbors' : True, # Parse relationships for States neighbor data.
+        'entities.states.diplomacy' : True, # Parse relationships for States diplomacy data.
+    'entities.states.diplomacy.all' : False, # Parse relationships for Neutral and Unknown diplomacies.  Probably leave this off.
+          'entities.states.history' : True, # Will parse history where appropriate 
+               'entities.provinces' : True, # Parse Provinces
                    'entities.burgs' : False, # Parse Burgs
 }
 kanka_api = {
